8f3801c920b4ca8600182c2945fc8c4e8bce9312
unknown <luqita.dmayo@gmail.com>
2023-12-22 11:01:52 -0300

    Instalo vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    target: 'esnext', // Puedes ajustar el objetivo de construcción según tus necesidades
+    outDir: 'dist',    // Directorio de salida para los archivos construidos
+    assetsDir: 'assets', // Directorio para activos estáticos
+    minify: 'terser',   // Minificar con Terser
+    sourcemap: true,    // Generar sourcemaps
+  },
+  server: {
+    port: 3000,         // Puerto para el servidor de desarrollo
+  },
+});
